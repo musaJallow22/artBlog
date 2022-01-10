0854ef36d33c589190bbd25a818bc43648ae10ec
musaJallow22 <56697206+musaJallow22@users.noreply.github.com>
2022-01-10 12:10:32 -0500

    More MS2 work.
    Working on dynamic route and index page.

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html');
   response.render("index");
+
 });
 //Creating our dynamic pages.
 app.get('/createaboutartists', function(request, response) {
@@ -32,68 +33,63 @@ app.get('/post', function(request, response) {
   response.setHeader('Content-Type', 'text/html');
   response.render("post");
 });
+//Dynamic route that links to posted content.
 app.get('/artpage/:titlename', function(request, response) {
   let artPosts = JSON.parse(fs.readFileSync('data/artposts.json'));
   let titlename = request.params.titlename;
 
-  if(artPosts[titlename]){
+  if (artPosts[titlename]) {
 
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html');
-  response.render("artpage", {
-    art: artPosts[titlename]
-  });
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html');
+    response.render("artpage", {
+      art: artPosts[titlename]
+    });
 
-}
-else{
+  } else {
     response.status(404);
     response.setHeader('Content-Type', 'text/html')
     response.render("error", {
-      "errorCode":"404"
+      "errorCode": "404"
     });
   }
 });
 
 //Posting to post page.
-app.post('/post', function(request, response){
+app.post('/post', function(request, response) {
   let titlename = request.body.titlename;
   let artistname = request.body.artistname;
   let datetime = request.body.datetime;
   let photolink = request.body.photolink;
   let artiststatement = request.body.artiststatement;
-  if(titlename && artistname && datetime && photolink && artiststatement){
+  if (titlename && artistname && datetime && photolink && artiststatement) {
     let artPosts = JSON.parse(fs.readFileSync('data/artposts.json'));
     let artPost = {
-      "title":titlename,
-      "artist":artistname,
-      "date":datetime,
-      "photo":photolink,
-      "statement":artiststatement,
+      "title": titlename,
+      "artist": artistname,
+      "date": datetime,
+      "photo": photolink,
+      "statement": artiststatement,
     }
 
-  artPosts[titlename] = artPost;
+    artPosts[titlename] = artPost;
 
 
-  fs.writeFileSync('data/artposts.json',JSON.stringify(artPosts));
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html');
-  response.redirect("/artpage/" + titlename);
+    fs.writeFileSync('data/artposts.json', JSON.stringify(artPosts));
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html');
+    response.redirect("/artpage/" + titlename);
 
-  }
-  else{
-      response.status(400);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("error", {
-        "errorCode":"400"
-      });
+  } else {
+    response.status(400);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("error", {
+      "errorCode": "400"
+    });
   }
 });
 
-app.get('/artpage', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html');
-  response.render("artpage");
-});
+
 
 app.get('/aboutartists', function(request, response) {
   response.status(200);
@@ -108,16 +104,16 @@ app.get('/comments', function(request, response) {
 });
 
 //In case of an error or invalid path.
-app.use("", function(request, response){
+app.use("", function(request, response) {
   response.status(404);
   response.setHeader('Content-Type', 'text/html')
   response.render("error", {
-    "errorCode":"404"
+    "errorCode": "404"
   });
 });
 
 //Start server.
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('Server started at http://localhost:'+port+'.')
+  console.log('Server started at http://localhost:' + port + '.')
 });
